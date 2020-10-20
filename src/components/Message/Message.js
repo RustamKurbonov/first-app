d55cc67a496540f757b3d16665e2cf45342aa06c
Rustam <kurbonovrustam47@gmail.com>
2020-10-20 15:35:17 +0300

    modified Messange and Posts

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,24 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Style from './Message.module.css'
 import MessageList from './MessageList/MessageList'
 import UserItem from './UserItem/UserItem'
 
-export default class Message extends Component {
-   render() {
-      return (
-         <section className={Style.messages}>
-            <div className={Style.users_list}>
-               <UserItem name='Alexey' id='1' />
-               <UserItem name='Dima' id='2' />
-               <UserItem name='Ric' id='3' />
-            </div>
-            <div className={Style.message_list}>
-               <MessageList name='Alexey' message='Hi' />
-               <MessageList name='Me' message='Hi!' />
-               <MessageList name='Alexey' message='lorem lorem ckmnc jdnwekn ejvdovnwj nkwlneufne jnkjn wenfjnlnf nvj nwoefnlnf' />
-            </div>
-         </section>
-      )
-   }
+export default function Message(props) {
+   let users = [
+      { name: 'Alexey', id: 1 },
+      { name: 'Dima', id: 2 },
+      { name: 'Ric', id: 3 }
+   ];
+   let messangs = [
+      { name: 'Alexey', messange: 'Hi'},
+      { name: 'Me', messange: 'Hi!' },
+      { name: 'Alexey', messange: 'nkjn wenfjnlnf nvj nwoefnlnf' }
+   ];
+   let usersElement = users.map(u => <UserItem name={u.name} id={u.id} />);
+   let massengElement = messangs.map(m => <MessageList name={m.name} message={m.messange} />);
+   return (
+      <section className={Style.messages}>
+         <div className={Style.users_list}>
+            {usersElement}
+         </div>
+         <div className={Style.message_list}>
+            {massengElement}
+         </div>
+      </section>
+   )
 }
-
--- src/components/Profile/Posts/Posts.js
+++ src/components/Profile/Posts/Posts.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Post from './Post/Post'
 import Style from './Posts.module.css'
-export default class Posts extends Component {
-   render() {
-      return (
-         <div className={Style.body}>
-            <div className={Style.container}>
-               <h2>Мои новости</h2>
-               <div className={Style.top}>
-                  <textarea placeholder="Что нового?" cols="30" rows="5" className={Style.text}></textarea>
-                  <button className={Style.btn}>Add post</button>
-               </div>
-               <div className={Style.bottom}>
-                  <Post nameUser='Alexey' message='Это я'/>
-                  <Post nameUser='Andrey' message='Привет, это второй пост'/>
-                  <Post nameUser='Dima' message='Еще один пост'/>
-               </div>
+
+export default function Posts(props) {
+   let posts = [
+      { name: 'Alexey', message: 'Это я' },
+      { name: 'Andrey', message: 'Привет, это второй пост' },
+      { name: 'Dima', message: 'Еще один пост' }
+   ];
+
+   let postElement = posts.map(p => <Post nameUser={p.name} message={p.message} />);
+   
+   return (
+      <div className={Style.body}>
+         <div className={Style.container}>
+            <h2>Мои новости</h2>
+            <div className={Style.top}>
+               <textarea placeholder="Что нового?" cols="30" rows="5" className={Style.text}></textarea>
+               <button className={Style.btn}>Add post</button>
+            </div>
+            <div className={Style.bottom}>
+               { postElement}
             </div>
          </div>
-      )
-   }
+      </div>
+   )
 }
+
